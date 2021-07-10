48ff6108e93b155ceebdad842eaa8c909255d51c
Solai Raj <msraj085@gmail.com>
2021-07-10 17:29:17 +0530

    code cleanup

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import ServerStyleSheets from "@material-ui/styles/ServerStyleSheets/ServerStyleSheets";
 
 export default class MyDocument extends Document {
   render() {
@@ -17,23 +16,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-
-MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
-};
