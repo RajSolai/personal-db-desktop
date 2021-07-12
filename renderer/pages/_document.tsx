5d524ab1b3df90810f6e74df4c668d7b1d64d178
Solai Raj <msraj085@gmail.com>
2021-07-12 11:54:15 +0530

    added inter font

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -7,6 +7,12 @@ export default class MyDocument extends Document {
       <Html lang="en" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
