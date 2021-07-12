b22775e668aff0990b3ee99209146684b680f2a0
Solai Raj <msraj085@gmail.com>
2021-07-12 16:46:27 +0530

    added section input

--- renderer/pages/notes/index.tsx
+++ renderer/pages/notes/index.tsx
@@ -12,23 +12,29 @@ const Notes: React.FC<any> = (props: any) => {
       event.preventDefault();
       if (keyword.trim() === "h1/") {
         const node: HTMLHeadingElement = document.createElement("h1");
-        node.className = "text-2xl font-bold";
+        node.className = "block text-2xl font-bold";
         const textNode = document.createTextNode("New H1 Title");
         node.appendChild(textNode);
         parentRef.current.appendChild(node);
       } else if (keyword.trim() === "h2/") {
         const node: HTMLHeadingElement = document.createElement("h2");
-        node.className = "text-xl font-bold";
+        node.className = "block text-xl font-bold";
         const textNode = document.createTextNode("New H2 Title");
         node.appendChild(textNode);
         parentRef.current.appendChild(node);
-      } else if (keyword.trim() === "ul/") {
-        const node: HTMLUListElement = document.createElement("ul");
-        const listItem:HTMLLIElement = document.createElement("li");
-        node.className = "text-base italic";
-        const textNode = document.createTextNode("New H2 Title");
-        node.appendChild(textNode);
+      } else if (keyword.trim() === "d/") {
+        const node: HTMLHRElement = document.createElement("hr");
+        const paraNode: HTMLParagraphElement = document.createElement("p");
+        const divNode: HTMLDivElement = document.createElement("div");
+        const headNode: HTMLHeadingElement = document.createElement("h2");
+        const headingText = document.createTextNode("New Section");
+        paraNode.innerText = "Enter the New Block";
+        headNode.className = "block text-xl font-bold";
         parentRef.current.appendChild(node);
+        headNode.appendChild(headingText);
+        divNode.appendChild(headNode);
+        divNode.appendChild(paraNode);
+        parentRef.current.appendChild(divNode);
       } else {
         console.log("fuck");
       }
